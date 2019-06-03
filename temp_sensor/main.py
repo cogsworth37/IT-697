f0ff81669b9354126eab97740ca1bf3151857723
Andy <andy@Andys-MacBook-Pro.local>
2019-06-02 22:10:11 -0500

    Pushing to Mqtt Listener

--- temp_sensor/main.py
+++ temp_sensor/main.py
@@ -2,11 +2,13 @@ from grovepi import *
 from grove_rgb_lcd import *
 import time
 from functions import *
+from mqtt.mqtt_client import MqttClient
 
 dht_sensor_port = 7
 
 while True:
   try:
+    mqtt = MqttClient("localhost")
     time.sleep(1)
     [ temp,hum ] = dht(dht_sensor_port,0)		#Get the temperature and Humidity from the DHT sensor
     #print("temp =", temp, "C\thumidity =", hum,"%") 
@@ -15,6 +17,7 @@ while True:
     bgColors = setBackgroundColor(temp) 
     setRGB(bgColors['red'], bgColors['green'], bgColors['blue'])
     setText_norefresh(output)
+    mqtt.post('hello/world', output)
   except KeyboardInterrupt:
     setRGB(100,100,100)
     setText("Goodbye!")
