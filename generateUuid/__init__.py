652209c62c7648ce970e5b38b006731835c3fa62
Andy <andy@Andys-MacBook-Pro.local>
2019-07-21 19:33:01 -0500

    Week 8 stuff

--- generateUuid/__init__.py
+++ generateUuid/__init__.py
@@ -3,5 +3,10 @@ import netifaces
 
 
 def generateUuid(name="", domain=".snhu.edu"):
+    """ Generate a device specific Type 5 UUID within a namespace and domain.
+    :param namespace: The namespace where the UUID is being generated
+    :param domain: The domain where the UUID is being generated
+    :return: Type 5 UUID
+    """
     mac = netifaces.ifaddresses("eth0")[netifaces.AF_LINK][0]["addr"].encode("utf-8")
     return str(uuid.uuid5(uuid.NAMESPACE_DNS, mac + "." + name + domain))
